51c053888503170d5a4a86e884ed34cbff90457e
rohitkatlaa <rohitkatlaa@gmail.com>
2022-04-09 22:05:55 +0530

    adding power and log operations

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -14,13 +14,15 @@ public class Calculator {
 
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
-        double num1, result;
+        double num1, num2, result;
         while(true) {
             System.out.println("----------------------------------------------------------------------");
             System.out.println("Scientific Calculator with DevOps , Choose an operation to calculate:");
             System.out.println("1. Square root");
             System.out.println("2. Factorial");
-            System.out.println("3. Exit");
+            System.out.println("3. Power");
+            System.out.println("4. Natural Log");
+            System.out.println("5. Exit");
             System.out.println("----------------------------------------------------------------------");
             System.out.print("Enter an option:");
             int option;
@@ -43,6 +45,18 @@ public class Calculator {
                     num1 = scanner.nextDouble();
                     result = calculator.factorial(num1);
                     System.out.println("Factorial of " + num1 + " is : " + result);
+                    break;
+                case 3:
+                    System.out.print("Enter the first number : ");
+                    num1 = scanner.nextDouble();
+                    System.out.print("Enter the second number : ");
+                    num2 = scanner.nextDouble();
+                    System.out.println(num1+ " raised to power "+num2+" is : " + calculator.power(num1, num2));
+                    break;
+                case 4:
+                    System.out.print("Enter a number : ");
+                    num1 = scanner.nextDouble();
+                    System.out.println("Natural log of "+num1+" is : " + calculator.naturalLog(num1));
                     break;
                 default:
                     System.out.println("Thank you for using Scientific Calculator with DevOps!!");
@@ -67,6 +81,32 @@ public class Calculator {
             result *= i;
         }
         logger.info("[RESULT - FACTORIAL] - " + result);
+        return result;
+    }
+
+    public double power(double num1, double num2) {
+        logger.info("[POWER - " + num1 + " RAISED TO] " + num2);
+        double result = Math.pow(num1,num2);
+        logger.info("[RESULT - POWER] - " + result);
+        return result;
+    }
+
+    public double naturalLog(double num1) {
+        logger.info("[NATURAL LOG] - " + num1);
+        double result = 0;
+        try {
+
+            if (num1 <0 ) {
+                result = Double.NaN;
+                throw new ArithmeticException("Case of NaN 0.0/0.0");
+            }
+            else {
+                result = Math.log(num1);
+            }
+        } catch (ArithmeticException error) {
+            System.out.println("[EXCEPTION - LOG] - Cannot find log of negative numbers " + error.getLocalizedMessage());
+        }
+        logger.info("[RESULT - NATURAL LOG] - " + result);
         return result;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -16,4 +16,14 @@ public class CalculatorTest {
     public void factorial_test(){
         assertEquals("Testing factorial of 5", 120, calc.factorial(5), DELTA);
     }
+
+    @Test
+    public void power_test(){
+        assertEquals("Testing power of 5 raised to 2", 25, calc.power(5, 2), DELTA);
+    }
+
+    @Test
+    public void log_test(){
+        assertEquals("Testing log of 1", 0, calc.naturalLog(1), DELTA);
+    }
 }
